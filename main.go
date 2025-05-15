4c4323b62bceb11367c2c5ff6ae002037cdbcce8
Ben Vaughan <ben@benvon.net>
2025-05-14 19:54:59 -0500

    Implement versioning support in CLI tool (#4)
    - Added version, commit, and build date variables to the main application.
    - Updated the root command to print version information when the --version flag is used.
    - Introduced unit tests for version flag functionality and help output in root_test.go.
    - Configured GoReleaser to specify the binary name for builds.

--- main.go
+++ main.go
@@ -7,9 +7,20 @@ import (
 	"github.com/benvon/testrigor-ci-tool/cmd"
 )
 
+var (
+	version string
+	commit  string
+	date    string
+)
+
 func main() {
+	// Set version information in the root command
+	cmd.Version = version
+	cmd.Commit = commit
+	cmd.Date = date
+
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-} 
+}
